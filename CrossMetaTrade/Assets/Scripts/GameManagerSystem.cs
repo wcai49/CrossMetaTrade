7f32878031f609dfae47b687dbc6b758053a4dfa
Wenlei Cai <35846257+wcai49@users.noreply.github.com>
2022-08-29 13:24:35 +0800

    Add cinemachine and TPS control

---
+++ CrossMetaTrade/Assets/Scripts/GameManagerSystem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerSystem : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+}
--- CrossMetaTrade/Assets/Scripts/PlayerControl.cs
+++ CrossMetaTrade/Assets/Scripts/PlayerControl.cs
@@ -7,17 +7,15 @@ public class PlayerControl : MonoBehaviour
     // player movement
     public float moveSpeed = 5f;
     public Vector2 moveInput = new Vector2(0f, 0f);
-            
-    // camera control
-    
-    [SerializeField] float sensitivity = 8.0f;
-    
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
     // sign components
     public CharacterController characterController;
+    public Transform cam;
     Animator playerAnimator;
     PlayerControlAction playerControlAction;
-    Camera playerCamera;
-   
+    AudioSource footStepSound;
+
     private void Awake()
     {
         playerControlAction = new PlayerControlAction();
@@ -44,43 +42,32 @@ public class PlayerControl : MonoBehaviour
         // assign gameobjects
         characterController = GetComponent<CharacterController>();
         playerAnimator = GetComponentInChildren<Animator>();
+        footStepSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(moveInput.magnitude != 0)
+        if (moveInput.magnitude != 0)
         {
             playerAnimator.SetBool("isWalking", true);
-            // player is moving backwards
-            if (moveInput.y < 0)
+            if (!footStepSound.isPlaying)
             {
-                float backwardSlow = 0.3f;
-                characterController.Move(-transform.forward * moveSpeed * backwardSlow * Time.deltaTime);
+                footStepSound.Play();
+            }
+            Vector3 direction = new Vector3(moveInput.x, 0, moveInput.y).normalized;
 
-            }
-            // player strafe left or right
-            else if (moveInput.x != 0)
-            {
-                if (moveInput.x < 0)
-                {
-                    characterController.Move(-transform.right * moveSpeed  * Time.deltaTime);
-                }
-                else
-                {
-                    characterController.Move(transform.right * moveSpeed * Time.deltaTime);
-                }
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            }
-            // player move forward
-            else
-            {
-                characterController.Move(transform.forward * moveSpeed  * Time.deltaTime);
-            }
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
         }
         else
         {
             playerAnimator.SetBool("isWalking", false);
+            footStepSound.Stop();
         }
-    }    
+    }
 }
