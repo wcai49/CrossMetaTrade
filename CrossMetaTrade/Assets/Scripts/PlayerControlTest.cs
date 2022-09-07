b7c6ed01a753a82f8a78e7795aab038fd8a10844
Wenlei Cai <35846257+wcai49@users.noreply.github.com>
2022-09-07 23:54:37 +0800

    Add test scene for playercontrol fix; Add city packages.

---
+++ CrossMetaTrade/Assets/Scripts/PlayerControlTest.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlTest : MonoBehaviour
+{
+    // player movement
+    public float moveSpeed = 5f;
+    public Vector2 moveInput = new Vector2(0f, 0f);
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    // sign components
+    public CharacterController characterController;
+    public Transform cam;
+    Animator playerAnimator;
+    PlayerControlAction playerControlAction;
+    AudioSource footStepSound;
+
+    private void Awake()
+    {
+        playerControlAction = new PlayerControlAction();
+        // bind WASD movement
+        playerControlAction.Player.Move.performed += moveValue => moveInput = moveValue.ReadValue<Vector2>();
+        playerControlAction.Player.Move.canceled += moveValue => moveInput = moveValue.ReadValue<Vector2>();
+
+        cam = Camera.main.transform;
+    }
+
+    #region - Enable / Disable -
+    private void OnEnable()
+    {
+        playerControlAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControlAction.Disable();
+    }
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        // assign gameobjects
+        characterController = GetComponent<CharacterController>();
+        playerAnimator = GetComponentInChildren<Animator>();
+        footStepSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (moveInput.magnitude != 0)
+        {
+            playerAnimator.SetBool("isWalking", true);
+            if (!footStepSound.isPlaying)
+            {
+                footStepSound.Play();
+            }
+            Vector3 direction = new Vector3(moveInput.x, 0, moveInput.y).normalized;
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            playerAnimator.SetBool("isWalking", false);
+            footStepSound.Stop();
+        }
+
+
+    }
+}
