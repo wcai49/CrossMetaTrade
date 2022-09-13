492ddb583438e5a0c636314aaf720c9f6ab40d30
Wenlei Cai <35846257+wcai49@users.noreply.github.com>
2022-09-13 23:14:29 +0800

    Trying to fix photon connect problem.

--- CrossMetaTrade/Assets/Scripts/LobbyManager.cs
+++ CrossMetaTrade/Assets/Scripts/LobbyManager.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
     public InputField lobbyInput;
-
     public void CreateRoom()
     {
         PhotonNetwork.CreateRoom(lobbyInput.text);
@@ -15,11 +15,24 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     public void JoinRoom()
     {
+        Debug.Log("Joining");
         PhotonNetwork.JoinRoom(lobbyInput.text);
     }
 
     public override void OnJoinedRoom()
     {
+        Debug.Log("Joined");
         PhotonNetwork.LoadLevel("MetaWorld");
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log(returnCode);
+        Debug.Log(message);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        Debug.Log(roomList);
     }
 }
