36059eff65606612af32ceb7821287e7b785a90d
Wenlei Cai <35846257+wcai49@users.noreply.github.com>
2022-09-22 23:35:04 +0800

    Finish trading scenario.

---
+++ CrossMetaTrade/Assets/NFTs.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new NFT", menuName = "nft")]
+public class NFTs : ScriptableObject
+{
+    public Sprite NFT_Image;
+    public string Details;
+    public string nft_name;
+    public string ownership;
+    public int price;
+    public Sprite Currency_icon;
+    public Sprite graph;
+    public string offers;
+
+
+    public void AddOffers(string newOffer)
+    {
+        offers += " " + newOffer;
+    }
+}
